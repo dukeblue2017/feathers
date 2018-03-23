ed075560e05045f138c3202ef6f7469d4cf9ebab
Carter Duncan <dukeblue2017@gmail.com>
2018-03-23 14:14:48 -0500

    Build out Login component
    Further work on client

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,10 +1,55 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+    };
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleUsernameChange(e) {
+    this.setState({
+      username: e.target.value
+    });
+    e.preventDefault();
+  }
+
+  handlePasswordChange(e) {
+    this.setState({
+      password: e.target.value
+    });
+    e.preventDefault();
+  }
+
+
+  handleSubmit(e) {
+    e.preventDefault();
+    axios.post({
+      
+    })
+  }
+
+
   render() {
     return (
       <div className="login">
-      hi
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Username
+            <input value={this.state.username} onChange={this.handleUsernameChange} />
+          </label>
+          <label>
+            Password
+            <input value={this.state.password} onChange={this.handlePasswordChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
       </div>
     );
   }
