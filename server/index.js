23621eba630bf0bcc8cd2171c194969d4f49da9e
Carter Duncan <dukeblue2017@gmail.com>
2018-03-25 00:33:52 -0500

    Set up login framework

--- server/index.js
+++ server/index.js
@@ -3,35 +3,30 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-// const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const sessions = {}; // not for use in production
 
 app.listen(8000, () => console.log('Server running on port 8000'));
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
-// app.use('/', express.static(path.join(__dirname, '/../client/build')))
-
-// app.use(session({
-//   secret: 'quailfeather',
-//   saveUninitialized: true,
-//   resave: true,
-//   cookie: { maxAge: 100 * 60 * 60 * 5 },
-//   proxy: undefined,
-// }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
-  // if logged in, send them the app
-  // if not, redirect them to login page
-  res.end();
+  if (req.cookies.sessID && sessions[req.cookies.username] === req.cookies.sessID) {
+    res.send('app here');
+  } else {
+    res.redirect('/login');
+  }
 });
 
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname, '/static/login.html'));
-});
-
-app.get('/signup', (req, res) => {
-  res.sendFile(path.join(__dirname, '/static/signup.html'));
+  if (req.cookies.sessID && sessions[req.cookies.username] === req.cookies.sessID) {
+    res.redirect('/');
+  } else {
+    res.sendFile(path.join(__dirname, '/static/login.html'));
+  }
 });
 
 app.post('/login', (req, res) => {
@@ -39,21 +34,32 @@ app.post('/login', (req, res) => {
     username: req.body.username,
     password: req.body.password,
   };
-  if (true /*correct*/) {
+  if (req.cookies.sessID && sessions[req.cookies.username] === req.cookies.sessID) {
+    res.redirect('/');
+  } else if (true /*correct password*/) {
     const alphanumerics = 'abcdefghijklmnopqrstuvwxyz0123456789'.split('');
     let newID = '';
     for (let i = 0; i < 30; i += 1) {
       const random = Math.floor(Math.random() * 35.9999999);
       newID = newID.concat(alphanumerics[random]);
-    }
-    res.send('logged in');
+    } sessions[userSubmission.username] = newID;
+    res.cookie('sessID', newID);
+    res.cookie('username', userSubmission.username).redirect('/')
   } else {
-    // what if password is incorrect
+    res.status(401).send('Incorrect credentials.');
   }
-  res.end();
 });
 
-app.get('/home', (req, res) => {
-  console.log(req)
-  res.send('hello')
-})
+app.get('/signup', (req, res) => {
+  res.sendFile(path.join(__dirname, '/static/signup.html'));
+});
+
+app.use((req, res, next) => {
+  if (req.cookies.sessID && sessions[req.cookies.username] === req.cookies.sessID) {
+    next();
+  } else {
+    res.clearCookie('username');
+    res.clearCookie('sessID');
+    res.redirect('/')
+  }
+});
